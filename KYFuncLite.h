74c00fea25a8fd7ba8be6b4b50a02cf97a6e8b5d
KeithLin7024 <keithlin724@gmail.com>
2022-11-17 01:29:17 +0800

    add format string function

--- KYFuncLite.h
+++ KYFuncLite.h
@@ -315,6 +315,47 @@ namespace KYFuncLite {
 		return res;
 	}
 
+	namespace format {
+		template<typename ... Args>
+		static std::string str_format(const std::string& format, Args ... args) {
+			auto size_buf = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1;
+
+			if (size_buf <= 0) {
+				throw std::runtime_error("Error during formatting.");
+			}
+
+			auto size_buf64 = static_cast<size_t>(size_buf);
+			std::unique_ptr<char[]> buf(new(std::nothrow) char[size_buf64]);
+
+			if (!buf) {
+				return std::string("");
+			}
+
+			std::snprintf(buf.get(), size_buf64, format.c_str(), args ...);
+			return std::string(buf.get(), buf.get() + size_buf64 - 1);
+		}
+
+		template<typename ... Args>
+		static std::wstring wstr_format(const std::wstring& format, Args ... args) {
+			auto size_buf = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1;
+
+			if (size_buf <= 0) {
+				throw std::runtime_error("Error during formatting.");
+			}
+
+			auto size_buf64 = static_cast<size_t>(size_buf);
+			std::unique_ptr<char[]> buf(new(std::nothrow) char[size_buf64]);
+
+			if (!buf) {
+				return std::wstring("");
+			}
+
+			std::snprintf(buf.get(), size_buf64, format.c_str(), args ...);
+			return std::wstring(buf.get(), buf.get() + size_buf64 - 1);
+		}
+
+	}
+
 }
 
 #endif // !__KYFUNCLITE_H__
